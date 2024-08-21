99fea9cc48f73d80f1e63de4889a0966c5fa4631
Soulstorm <teddy.cozette@hotmail.fr>
2024-08-21 16:24:22 +0200

    Création de oscars.php pour la nouvelle section + style.scss et autre fichier CSS pour le style du site, les fade des sections, etc...

---
+++ wp-content/themes/foce-child/partial-parts/oscars.php
@@ -0,0 +1,6 @@
+<section class="oscars">
+    <div class="oscars__container">
+        <h3><span>Fleurs d'oranger & chats errants<br>est nominé aux Oscars Short<br>Film Animated de 2022 !</span></h3>
+        <img src="<?php echo esc_url(get_stylesheet_directory_uri() . '/new-image/oscars.png'); ?>" alt="Logo des Oscars Short Film Animated">
+    </div>
+</section>
